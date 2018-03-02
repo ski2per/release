082b8e87b0f4829df570e3d1e36d2771f968b5ed
ted <ski2per@163.com>
2018-03-02 20:56:52 +0800

    add .gitignore and setup_docker_centos.sh

---
+++ setup_docker_centos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###### CREATOR          Ted       
+###### DESCRIPTION      Environment setup
+###### VERSION          v1.0
+###### UPDATE           2018/02/26
+
+# THIS SCRIPT IS TESTED ON CENTOS 7.4
+# IT CAN BE USED TO SETUP THE ENVIRONMENT FOR DEVELOPERS
+
+# Prerequisites
+setenforce 0
+systemctl stop firewalld
+# Install required packages.
+yum -y install yum-utils device-mapper-persistent-data lvm2
+# Enable ip forward
+sysctl -w net.ipv4.ip_forward=1
+
+# Setup Docker
+DOCKER_REPO_URL=https://download.docker.com/linux/centos/docker-ce.repo
+
+# Configure Docker repo
+yum-config-manager --add-repo $DOCKER_REPO_URL
+# Install Docker
+yum -y install docker-ce
+systemctl enable docker
+systemctl start docker
