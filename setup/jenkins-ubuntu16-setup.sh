6bfc6fee59696d1631230de08f7820b55b4c538e
ted <ski2per@163.com>
2018-05-03 13:57:29 +0800

    add jenkins-ubuntu16-setup.sh

---
+++ setup/jenkins-ubuntu16-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###### CREATOR          Ted       
+###### DESCRIPTION      Setup Jenkins on Ubuntu 16.04
+###### VERSION          v1.0
+###### UPDATE           2018/05/03
+
+# TESTED ON UBUNTU 16.04
+
+JENKINS_DEBIAN_KEY="https://pkg.jenkins.io/debian-stable/jenkins.io.key"
+JENKINS_DEBIAN_SOURCE="deb https://pkg.jenkins.io/debian-stable binary/"
+# Add Jenkins key
+curl -s $JENKINS_DEBIAN_KEY | sudo apt-key add -
+
+# Add Jenkins source
+sudo echo $JENKINS_DEBIAN_SOURCE >> /etc/apt/sources.list
+
+# Install Jenkins
+sudo apt-get update
+sudo apt-get install jenkins
