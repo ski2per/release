884472ce30618fc9103e0fa80c0ec7fee90056f8
Ted <ski2per@163.com>
2018-05-17 20:08:03 +0800

    finish openvpn setup script

--- setup/openvpn-centos-setup.sh
+++ setup/openvpn-centos-setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+###### CREATOR          Ted       
+###### DESCRIPTION      Setup openvpn client on CentOS
+###### VERSION          v1.0
+###### UPDATE           2018/05/17
+
+SOURCE="openvpn-2.4.6.tar.gz"
+
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin"
 
-if [ -x /bin/curlx ];then
-    echo "ok"
-else
-    echo "not ok"
-fi
+# Install prerequisites
+yum -y install gcc openssl-devel lzo-devel pam-devel
 
-yum -y install openssl-devel lzo-devel pam-devel
+tar -zxvf $SOURCE
+
+folder=${SOURCE/.tar.gz/}
+cd $folder
+./configure
+make && make install
+cd ..
+
+rm -rf $folder
