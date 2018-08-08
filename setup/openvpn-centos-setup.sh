8d45a5eb89506380d2437ea9c87a55a9ea944856
Ted <ski2per@163.com>
2018-08-09 00:15:21 +0800

    update openvpn setup script

--- setup/openvpn-centos-setup.sh
+++ setup/openvpn-centos-setup.sh
@@ -2,9 +2,10 @@
 
 ###### CREATOR          Ted       
 ###### DESCRIPTION      Setup openvpn client on CentOS
-###### VERSION          v1.0
-###### UPDATE           2018/05/17
+###### VERSION          v1.1
+###### UPDATE           2018/08/09
 
+# Download openvpn sourcecode first
 SOURCE="openvpn-2.4.6.tar.gz"
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin"
