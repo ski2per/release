2889a3cc7d2086bb8732b35f80f8936e34f0a350
zhangpeng <ski2per@163.com>
2018-11-20 19:39:59 +0800

    bug fix

--- docker/stack/cadvisor-influx-grafana/cadvisor/setup.sh
+++ docker/stack/cadvisor-influx-grafana/cadvisor/setup.sh
@@ -3,31 +3,43 @@
 # docker-compose version 1.10+
 # ============================================================================
 
+#set -x
+
 # Enable Docker Engine API
 loaded=$(systemctl status docker | grep -i 'oaded:' | awk -F'(' '{print $2}')
 docker_service=${loaded%%;*}
-
-echo "Save current running containers..."
-running_containers=$(docker ps -q --filter status=running)
-
+echo $docker_service
 
 if [ -e "$docker_service" ];then
-    sed -i 's#^ExecStart=.*#ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375#' $docker_service
-    echo "Enable Docker API and restart docker"
-    systemctl daemon-reload && systemctl restart docker
+    grep '0.0.0.0:2375' "$docker_service" &> /dev/null
+    if [ "$?" -ne 0 ];then
+        sed -i '/^ExecStart=/ s#$# -H tcp://0.0.0.0:2375#' $docker_service
 
-    echo "Restore containers"
-    for container in $running_containers;do
-        docker start $container
-    done
+        echo "Save current running containers..."
+        running_containers=$(docker ps -q --filter status=running 2> /dev/null)
+
+        echo "Enable Docker API and restart docker"
+        systemctl daemon-reload && systemctl restart docker
+
+        echo "Restore containers"
+        for container in $running_containers;do
+            docker start $container
+        done
+    fi
+
 else
     echo "Cannot found $docker_service"
     exit 2
 fi
 
-COMPOSE_BIN=$(which docker-compose)
 # Start Cadvisor
+COMPOSE_BIN=$(which docker-compose)
 if [ -x "$COMPOSE_BIN" ];then
+    # Start Docker service if it's stopped
+    docker ps &> /dev/null
+    if [ "$?" -ne 0 ];then
+        systemctl start docker
+    fi
     docker-compose up -d && echo "Cadvisor started..."
 else
     echo "Cannot found docker-compose"
