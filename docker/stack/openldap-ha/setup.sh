75f3c0516cd8e52bcdac09cd3f0cf21a3ef7a4c4
zhangpeng <ski2per@163.com>
2018-11-28 18:08:56 +0800

    update setup.sh

--- docker/stack/openldap-ha/setup.sh
+++ docker/stack/openldap-ha/setup.sh
@@ -1,24 +1,84 @@
 #!/bin/bash
 
+# set -x
 
-
-MODS=("cosine" "nis" "inetorgperson")
+INCLUDE_MODS=("cosine" "nis" "inetorgperson")
 INCLUDE_TPL='include: file:///usr/local/etc/openldap/schema/'
-TPL='include: file://'
+#SLAPD_LDIF=/usr/local/etc/openldap/slapd.ldif
+TMP_LDIF=/tmp/slapd.ldif
+SLAPD_LDIF=slapd.ldif
 
-cp /usr/local/etc/openldap/slapd.ldif /tmp
-line_no=$(grep -n "$TPL" /tmp/slapd.ldif | awk -F':' '{print $1}')
-
-for mod in ${MODS[@]};do
-    line="$INCLUDE_TPL$mod.ldif"
-    echo $line
-    sed -i -e "#${TPL}# i\"${line}\"" /tmp/slapd.ldif
-    #sed -i -e "${line_no}a${line}" /tmp/slapd.ldif
+function update_slapd_ldif {
+    tpl='include: file://'
     
-done
-env | grep 'LDAP'
+    if [ ! -e $SLAPD_LDIF ];then
+        echo "Can not found: $SLAPD_LDIF"
+        exit 1
+    fi
+
+    cp $SLAPD_LDIF $TMP_LDIF
+    line_no=$(sed -n "\#$tpl#{=;q}" $TMP_LDIF)
+
+    # Insert inclue lines in slapd.ldif
+    # ====================================
+    for mod in ${INCLUDE_MODS[@]};do
+        line="$INCLUDE_TPL$mod.ldif"
+        sed -i "$line_no a \\$line" $TMP_LDIF
+        ((line_no++))
+    done
+
+    # Replace domain in slapd.ldif
+    # ====================================
+
+    # Split domain component in array
+    dcs=(${LDAP_DOMAIN//./ })
+
+    for dc in ${dcs[@]};do
+         suffix="$suffix,dc=$dc"
+    done
+
+    suffix=${suffix#","}
+
+    olcSuffix_line="olcSuffix: $suffix"
+    olcRootDN_line="olcRootDN: cn=admin,$suffix"
+
+    line_no=$(sed -n "/olcSuffix/{=;q}" $TMP_LDIF)
+    sed -i "${line_no}d" $TMP_LDIF
+    # Replace olcSuffix line
+    sed -i "$line_no i \\$olcSuffix_line" $TMP_LDIF
+
+    line_no=$(sed -n "/olcRootDN/{=;q}" $TMP_LDIF)
+    sed -i "${line_no}d" $TMP_LDIF
+    # Replace olcRootDN line
+    sed -i "$line_no i \\$olcRootDN_line" $TMP_LDIF
 
 
-mkdir -p /usr/local/var/openldap-data
-mkdir -p /usr/local/etc/slapd.d
+    # Init admin password
+    # ====================================
+    SLAP_PASSWD_BIN=$(which slappasswd)
+    LDAP_ADMIN_PASSWORD=admin
+    if [ "$SLAP_PASSWD_BIN" == "" ];then
+        olcRootPW_line="olcRootPW: $LDAP_ADMIN_PASSWORD"
+    else
+        echo $SLAP_PASSWD_BIN
+        password_digest=$(slappasswd -n -s $LDAP_ADMIN_PASSWORD)
+        olcRootPW_line="olcRootPW: $password_digest"
+    fi
 
+    line_no=$(sed -n "/olcRootPW/{=;q}" $TMP_LDIF)
+    sed -i "${line_no}d" $TMP_LDIF
+    # Replace olcRootPW line
+    sed -i "$line_no i \\$olcRootPW_line" $TMP_LDIF
+}
+
+function debug {
+    echo "debug"
+}
+
+function main {
+    update_slapd_ldif
+    #debug
+}
+    #mkdir -p /usr/local/var/openldap-data
+    #mkdir -p /usr/local/etc/slapd.d
+main
