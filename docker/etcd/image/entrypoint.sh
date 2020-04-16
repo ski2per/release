6416142a91cb31f1362e5526fca493c8789fc7bd
Ted <ski2per@163.com>
2020-04-16 08:45:54 +0800

    move docker to repo d0cker

--- docker/etcd/image/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-etcd --name ${ETCD_NAME} --enable-v2=true \
-  --initial-advertise-peer-urls ${ETCD_INITIAL_ADVERTISE_PEER_URLS} \
-  --listen-peer-urls ${ETCD_LISTEN_PEER_URLS} \
-  --listen-client-urls ${ETCD_LISTEN_CLIENT_URLS} \
-  --advertise-client-urls ${ETCD_ADVERTISE_CLIENT_URLS} \
-  --initial-cluster-token ${ETCD_INITIAL_CLUSTER_TOKEN}\
-  --initial-cluster ${ETCD_INITIAL_CLUSTER}
-  --initial-cluster-state ${ETCD_INITIAL_CLUSTER_STATE}
