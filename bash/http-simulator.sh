6d6f599b95e5d25e98f5c0c1b1d5f08f5a314f3b
Ted <ski2per@gmail.com>
2020-11-23 23:03:40 +0800

    update

--- bash/http-simulator.sh
+++ bash/http-simulator.sh
@@ -6,17 +6,18 @@ HOSTS=(
 "c.ted.com"
 )
 
-function generate_rand_str {
-    RAND=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+function generate_rand {
+    RAND_STR=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+    RAND_NO=$(cat /dev/urandom | tr -dc '0-9' | fold -w 1 | head -n 1)
 }
 
 
 
 for host in "${HOSTS[@]}";do
-    echo $host
-    for i in {1..3};do
-        generate_rand_str
-        curl -s -o /dev/null -H "Host: $host" "http://127.0.0.1/$RAND"
+    echo "$host"
+    for ((i=0; i<=RAND_NO; i++));do
+        generate_rand
+        curl -s -o /dev/null -H "Host: $host" "http://127.0.0.1/$RAND_STR"
         #sleep 1
     done
 done
