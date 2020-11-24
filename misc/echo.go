d293b64cca8d1e42748b48b364428e250162b39d
Ted <ski2per@163.com>
2020-11-24 17:49:43 +0800

    add echo.go

---
+++ misc/echo.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "net/http"
+    "os"
+    "log"
+)
+
+const DefaultPort = "6666";
+
+func getServerPort() (string) {
+    port := os.Getenv("SERVER_PORT");
+    if port != "" {
+        return port;
+    }
+
+    return DefaultPort;
+}
+
+// EchoHandler echos back the request as a response
+func EchoHandler(writer http.ResponseWriter, request *http.Request) {
+
+    log.Println("Echoing back request made to " + request.URL.Path + " to client (" + request.RemoteAddr + ")")
+
+    writer.Header().Set("Access-Control-Allow-Origin", "*")
+    writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
+    request.Write(writer)
+}
+
+func main() {
+
+    log.Println("starting server, listening on port " + getServerPort())
+
+    http.HandleFunc("/", EchoHandler)
+    http.ListenAndServe(":" + getServerPort(), nil)
+}
